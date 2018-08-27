b866a8bdaf0a71b42044b310344e5b2a1c5a7dba
Sergey Arhipov <Sergey.Arhipov@arcelormittal.com>
2018-08-27 15:57:41 +0300

    fix filter

--- convert_avi_to_mp4.pl
+++ convert_avi_to_mp4.pl
@@ -16,88 +16,73 @@
  my $conf = configuration->new($log);
 
  my $DEBUG = $conf->get('app')->{'debug'};
- 
-
-my $dir = $conf->get('find')->{'directory'};
-
-#print Dumper($conf);
-print $conf->get('find')->{'ext'};
-
-find(\&wanted, $dir);
-
-my (@files, @dirs, %_files);
-my $d;
-my $dold;
-my $count = 0;
-
-sub wanted {
-			$d = $_ if -d $_;
-			if ( $d ne $dold ) {
-				$dold = $d;
-				print $d, " | ", $dold," n \n";
-				-d $_ and push @dirs, $File::Find::dir;
-				-f $_ and push @files, $File::Find::name;
-#				$_files{$count}{'dir'} = $File::Find::dir if -d $_;
-			} else {
-				$dold = $d;
-				print $d, " | ", $dold, " o \n";
-				-d $_ and push @dirs,  $File::Find::dir;
-				-f $_ and push @files, $File::Find::name;
-				#-f $_ and push @files, $File::Find::name =~ /(.*)\/(.*)$/;
-#				$_files{$count}{'dir'} = $File::Find::dir if -d $_;
-#				$File::Find::name =~ /(.*)\/(.*)\.(.*)$/ if -f $_;
-#				$_files{$count}{'dir'} = "$1/";
-#				$_files{$count}{'file'} = $2;
-#				$_files{$count}{'ext'} = $3;
-#				print $_files{$count}{'file'}, " | \n";
-#				-f $_ and $_files{'file'} = $File::Find::name =~ /(.*)\/(.*)$/;
-#				-f $_ and $_files{'ext'} = $File::Find::name =~ /(.*)\/(.*).(.*)$/;
-			}
-			$count++;
-#			print $d," n \n" if defined $d;
-#			print $dold, " o \n"; # if defined $dold and $dold eq 1;
-#            -f $_ and push @files, $File::Find::name;
-#            -f $_ and push @files, $File::Find::name;
-#            -d $_ and push @dirs,  $File::Find::name;
-};
-
-print Dumper(@dirs);
-print "\n\n";
-print Dumper(@files);
-print "\n\n";
-print Dumper(\%_files);
 
 
-my $match = $conf->get('find')->{'match_ext'};
+# while (1) {
+	my (@files, @dirs, $dir, $dir_old);
+	my $count = 0;
+	find(\&wanted, $conf->get('find')->{'directory'});
+		
+	&filter(@files);
+		
+    print "cycle: ",$conf->get('app')->{'cycle'}, "\n" if $DEBUG;
+#    select undef, undef, undef, $conf->get('app')->{'cycle'} || 10;
+# }
 
 
+ sub wanted {
+	$dir = $_ if -d $_;
+	if ( $dir ne $dir_old ) {
+		$dir_old = $dir;
+		print $dir, " | ", $dir_old," n \n" if $DEBUG;
+		-d $_ and push @dirs, $File::Find::dir;
+		-f $_ and push @files, $File::Find::name;
+	} else {
+		$dir_old = $dir;
+		print $dir, " | ", $dir_old, " o \n" if $DEBUG;
+		-d $_ and push @dirs,  $File::Find::dir;
+		-f $_ and push @files, $File::Find::name;
+	}
+	$count++;
+ };
 
-#=comm
-foreach my $file ( sort { $a cmp $b } @files ) {
-#if (my ($matched) = grep $_ =~ /$match/, @files) {
-#	print $file, "\n";
-	$file =~ /^(.*)\.(.*)$/;
-	my $_file = $1;
-	my $ext = $2;
-#	print my $ext = $2, "\n";
-#	if ( grep $file =~ /\.txt/ and ! $_ =~ /$match/, @files) {
-#		print $file, " |\n";
-#	}
-	#print $file, " |\n" if ($ext eq 'txt' and grep { $_file.$match ne $_ } @files );
+ sub filter {
+ 	my(@files) = @_;
 	
-	#print $file, " |\n" if ( $ext eq 'txt' and ! grep { $_file.$match ~~ /$_/g } @files );
-	&convert($_file) if ( $ext eq $conf->get('find')->{'ext'} and ! grep { $_file.$match ~~ /$_/g } @files );
+	print Dumper(@dirs);
+	print "\n\n";
+	print Dumper(@files);
+	print "\n\n";
 
-	#print $_file, " |+\n" if ( $file ne $_file.$match );
-#	print $_file.$match."\n";
+	my $match = $conf->get('find')->{'match_ext'};
+
+	#=comm
+	foreach my $file ( sort { $a cmp $b } @files ) {
+	#if (my ($matched) = grep $_ =~ /$match/, @files) {
+	#	print $file, "\n";
+		$file =~ /^(.*)\.(.*)$/;
+		my $_file = $1;
+		my $ext = $2;
+	#	print my $ext = $2, "\n";
+	#	if ( grep $file =~ /\.txt/ and ! $_ =~ /$match/, @files) {
+	#		print $file, " |\n";
+	#	}
+		#print $file, " |\n" if ($ext eq 'txt' and grep { $_file.$match ne $_ } @files );
+		
+		#print $file, " |\n" if ( $ext eq 'txt' and ! grep { $_file.$match ~~ /$_/g } @files );
+		&convert($_file) if ( $ext eq $conf->get('find')->{'ext'} and ! grep { $_file.$match ~~ /$_/g } @files );
+
+		#print $_file, " |+\n" if ( $file ne $_file.$match );
+	#	print $_file.$match."\n";
+	}
+
 }
-
 
 sub convert {
 	my($file) = @_;
 	
 	print $file, " |+\n";
-	system("ffmpeg.exe -i $file.".$conf->get('find')->{'ext'}." -vcodec copy ".$file.$conf->get('find')->{'match_ext'});
+#	system("ffmpeg.exe -i $file.".$conf->get('find')->{'ext'}." -vcodec copy ".$file.$conf->get('find')->{'match_ext'});
 	#D:\videoimages\ffmpeg>ffmpeg.exe -i D:\videoimages\video\2018\08\20\3503\cam1.avi -vcodec copy  D:\videoimages\video\2018\08\20\3503\cam1.mp4
 }
 
